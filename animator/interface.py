d58bae2d70f78cbd2746c22347451357c9eb0494
4g3nt81lly <billylby@yahoo.com>
2020-11-19 14:07:45 +0800

    Synchronization Protocol
    Sequence.A

--- animator/interface.py
+++ animator/interface.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
 from core import *
+from objects import *
 from geometry import *
 
 
@@ -8,14 +9,22 @@ class Window:
 
     __objects = {}
 
-    def add_shape(self, shape):
+    def add_shape(self, shape, identifier=None):
         name = f'create_{shape.suffix}({shape.args()})'
-        scope = {'canvas': self.canvas}
-        exec(f'object_id = canvas.{name}', scope)
-        self.__objects[scope['object_id']] = shape
+        scope = {'canvas': self.canvas, 'identifier': identifier}
+        exec(f'''object_id = canvas.{name}
+if identifier is not None:
+    canvas.itemconfig(object_id, tags=identifier)
+''', scope)
+        object_id = scope['object_id'] if identifier is None else identifier
+        self.__objects[object_id] = shape
 
-    def get_shape(self, id):
-        return self.__objects[id]
+    def remove_shape(self, identifier):
+        self.canvas.delete(identifier)
+        del self.__objects[identifier]
+
+    def run_animation(self, timeline: Timeline):
+        pass
 
     def __init__(self, frame: Rect = Rect(500, 300, Point(300, 300)),
                  title='Window'):
--- animator/objects.py
+++ animator/objects.py
@@ -1,15 +1,40 @@
-from geometry import *
 from core import *
+from geometry import *
 
 
-class Timeline:
-    __keyframes: [Keyframe] = []
+class Animation:
+    duration = 0.0
+    timing_funtion = None
+
+
+class TweeningAnimation(Animation):
+
+    def __init__(self, object_identifier: str):
+        super().__init__()
+        self.object_identifier = object_identifier
+
+
+class KeyframeAnimation(Animation):
+
+    values = []
+    key_times: [float] = []
+
+    def __init__(self, object_identifier: str):
+        super().__init__()
+        self.object_identifier = object_identifier
+
+
+class TransitionAnimation(Animation):
+    pass
+
+
+class AnimationGroup:
+    animations: [Animation] = []
 
 
 class Keyframe:
     __animations: [Animation] = []
 
 
-class Animation:
-    def __init__(self, object_identifier: str):
-        self.object_identifier = object_identifier
+class Timeline:
+    __keyframes: [Keyframe] = []
